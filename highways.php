13f2e5fa64c1d38aaa0c850480f6f9a22b6a21c1
Levend <levend-mohammad_student2020@wilder.school>
2020-04-18 17:10:03 +0200

    Update highways.php

--- highways.php
+++ highways.php
@@ -1,64 +1,90 @@
 <?php
+    abstract class HighWay{
 
-abstract class Vehicle
- {
-     /**
-      * @var string
-      */
-     protected $color;
+        protected $currentVehicles=array();
+        protected $nbLane;
+        protected $maxSpeed;
 
-
-
-
-     public function __construct(string $color)
-     {
-         $this->color = $color;
-     }
- }
-
-class Car extends Vehicle{
-
-
-
-
-    public function __construct($color)
+         public function __construct(int $nbLane, int $maxSpeed)
     {
-        parent::__construct($color);
-
+        $this->nbLane = $nbLane;
+        $this->maxSpeed = $maxSpeed;
     }
 
+        abstract public function addVehicle($vehicle);
+
+
+        public function getCurrentVehicles(){
+        return $this->currentVehicles;
+        }
+        public function setCurrentVehicles($currentVehicles) {
+        $this->currentVehicles =$currentVehicles;
+        }
+
+
+        public function getNbLane(){
+        return $this->nbLane;
+        }
+        public function setNbLane($nbLane){
+        $this->nbLane =$nbLane;
+        }
+
+
+        public function getMaxSpeed(){
+        return $this->maxSpeed;
+        }
+        public function setMaxSpeed($maxSpeed){
+        $this->maxSpeed= $maxSpeed;
+        }
+    }
+
+
+final class MotorWay extends HighWay{
+
+    public function __construct()
+    {
+        parent::__construct(4,130);
+    }
+        public function addVehicle($vehicle){
+            if($vehicle instanceof Car){
+             parent::setCurrentVehicles($vehicle);
+                return "your vehicle has been added";
+            }else{
+                return "can't be added";
+            }
+}
 }
 
 
-class Truck extends Vehicle{
 
+final class PedestrianWay extends HighWay{
 
-    public function __construct($color)
+    public function __construct()
     {
-        parent::__construct($color);
-    }
-}
-
-class Bike extends Vehicle
-{
-
-
-    public function __construct($color)
-    {
-        parent::__construct($color);
-    }
-}
-class Skatboard extends Vehicle
-{
-
-    public function __construct($color)
-    {
-        parent::__construct($color);
+        parent::__construct(1,10);
+    }   
+    public function addVehicle($vehicle){
+        if ($vehicle instanceof Bike || $vehicle instanceof Skateboard) {
+            parent::setCurrentVehicles($vehicle);
+            return "your vehicle has been added";
+        }else{
+            return "can't be added";
+        }
     }
 }
 
 
 
+final class ResidentialWay extends HighWay{
+    public function __construct()
+    {
+        parent::__construct(2,50);
+    }
 
+    public function addVehicle($vehicle){
+        $this->currentVehicles=$vehicle;
+        return "your vehicle has been added";
 
+    }
+}
 
